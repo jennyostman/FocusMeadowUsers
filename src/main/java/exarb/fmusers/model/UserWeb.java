523a1f0a44a79349c70214c3feeae27bca50d227
Jenny Östman <jenny.ostman@yh.nackademin.se>
2020-05-18 09:24:30 +0200

    Koll att inget fält är tomt vid registrering

--- src/main/java/exarb/fmusers/model/UserWeb.java
+++ src/main/java/exarb/fmusers/model/UserWeb.java
@@ -2,14 +2,21 @@ package exarb.fmusers.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class UserWeb {
 
     private String id;
+    @NotEmpty(message = "Name must not be empty")
     private String firstName;
+    @NotEmpty(message = "Name must not be empty")
     private String lastName;
+    @NotEmpty(message = "Usernamne must not be empty")
     private String userName;
+    @NotEmpty(message = "Email must not be empty")
     private String email;
+    @NotEmpty(message = "Password must not be empty")
     private String password;
 
 }
